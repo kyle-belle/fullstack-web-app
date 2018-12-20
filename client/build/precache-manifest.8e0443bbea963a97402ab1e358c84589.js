f416391ec7a84cc8f4ced80dbf373f3e0703e716
Kyle Belle <kylebellle7@gmail.com>
2018-12-20 04:46:02 -0400

    i

---
+++ client/build/precache-manifest.8e0443bbea963a97402ab1e358c84589.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b017565e092cff6a4f36",
+    "url": "/static/js/main.b017565e.chunk.js"
+  },
+  {
+    "revision": "6b8aa373c737129fe514",
+    "url": "/static/js/1.6b8aa373.chunk.js"
+  },
+  {
+    "revision": "b017565e092cff6a4f36",
+    "url": "/static/css/main.c09c46bc.chunk.css"
+  },
+  {
+    "revision": "af6b28d5d06df36c4d09d2d38133bf84",
+    "url": "/index.html"
+  }
+];
