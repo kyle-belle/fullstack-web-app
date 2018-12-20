6788cca82fdbf79af28d0bd9d4f74284f8f4f0c2
Kyle Belle <kylebellle7@gmail.com>
2018-12-20 05:18:37 -0400

    i

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ require("./routes/auth-routes")(app);
 require("./routes/stripe-routes")(app);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("/client/build"));
+    app.use(express.static("client/build"));
 
     const path = require("path");
     app.get("*", (req, res) => {
