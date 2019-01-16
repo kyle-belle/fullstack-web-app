30d91ad26de264bf344c84ad4c1fa0c129f85ed3
Kyle Belle <kylebellle7@gmail.com>
2019-01-16 14:41:56 -0400

    trying to play video on mobile

--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -261,17 +261,7 @@ class Landing extends Component {
                         <source src="laptop-typing-3.mp4" type="video/mp4" />
                     </video>
 
-                    <div className="video-overlay">
-
-                        <div className="video-text">Send gluten free surveys easier from anywhere with SurMail, The fastest proven way to send survey with 100%&#8482; response rate with Mind-Read&#8482; which guarantees all feedback provided is truthful, and with Moneyback&#8482; guarantee you can try Surmail risk free. Should this video be at the top? let me know at kylebellle7@gmail.com .</div>
-
-                        <div className="video-button-div">
-                            
-                            <button className="floating button green video-button">Learn How</button>
-                        
-                        </div>
-
-                    </div>
+                    
 
                 </div>
                 
